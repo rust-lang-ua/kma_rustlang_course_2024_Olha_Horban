59d1fad9d79e53c65b909e9ba8dc1e7543319f7d
Olha Horban <o.horban@epom.com>
2024-11-13 20:28:01 +0200

    completed section: enums + hashmap + modules + vec + strings

--- rustlings/exercises/collections/hashmap2.rs
+++ rustlings/exercises/collections/hashmap2.rs
@@ -12,11 +12,11 @@
 // Execute the command `rustlings hint hashmap2` if you need
 // hints.
 
-// I AM NOT DONE
+// I AM DONE
 
 use std::collections::HashMap;
 
-#[derive(Hash, PartialEq, Eq)]
+#[derive(Hash, PartialEq, Eq, Clone)]
 enum Fruit {
     Apple,
     Banana,
@@ -38,6 +38,12 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Put new fruits if not already present. Note that you
         // are not allowed to put any type of fruit that's already
         // present!
+        basket.entry(fruit.clone()).or_insert(0);
+        match fruit {
+            Fruit::Banana => *basket.entry(Fruit::Banana).or_insert(0) += 10,
+            Fruit::Pineapple => *basket.entry(Fruit::Pineapple).or_insert(0) += 10,
+            _ => {}
+        }
     }
 }
 
