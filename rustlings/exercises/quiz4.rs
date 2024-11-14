f9f8303be143f5f6220337387d77b69420488833
Olha Horban <o.horban@epom.com>
2024-11-14 07:31:53 +0200

    completed section: arc+box+iterarors+macros+threafs

--- rustlings/exercises/quiz4.rs
+++ rustlings/exercises/quiz4.rs
@@ -5,7 +5,13 @@
 
 // Write a macro that passes the quiz! No hints this time, you can do it!
 
-// I AM NOT DONE
+// I AM DONE
+
+macro_rules! my_macro {
+    ($arg:expr) => {
+        format!("Hello {}", $arg)
+    };
+}
 
 #[cfg(test)]
 mod tests {
